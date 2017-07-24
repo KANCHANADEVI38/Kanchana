6251247cd54bdcc0278efce3a35ba2cf93904bac
KANCHANADEVI38 <30399523+KANCHANADEVI38@users.noreply.github.com>
2017-07-24 14:31:45 +0530

    Add files via upload

---
+++ devi2.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class devi2 {
+
+	public static void main(String[] args) {
+		  int x = 123456789;
+
+		    String x_str = Integer.toString(x);
+
+		    int new_x = Integer.parseInt(x_str.substring(1));
+
+		    System.out.println(new_x);
+		}
+
+	}
+
+
