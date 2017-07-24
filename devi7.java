6bf2613d204b88db465797d7899663cc4cdaa6cf
KANCHANADEVI38 <30399523+KANCHANADEVI38@users.noreply.github.com>
2017-07-24 15:06:01 +0530

    Add files via upload

---
+++ devi7.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class devi7 {
+	
+	public static void main(String[] args, int i) {
+		String s="hello";
+		for( i=0;i<=s.length();i++)
+		{
+		    char e=s.charAt(i);
+		    if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U')){
+		        {
+		String h=s.substring(0, i);
+
+		String g=s.substring(i+ i);
+		s=h+"Z"+g;
+		}}
+		}
+		    System.out.println("The result is="+s);
+		}}
+
