f23be1434c962d2b2c0900b63d46785c16d9015c
KANCHANADEVI38 <30399523+KANCHANADEVI38@users.noreply.github.com>
2017-07-24 11:55:21 +0530

    Add files via upload

--- star.java
+++ star.java
@@ -2,23 +2,58 @@ package sample;
 import java.util.Scanner;
 public class star {
 	public static void main(String[] args){
-		int mainArray[] = { 1, 2, 3, 2, 5, 6, 2 }, subArray[] = { 2, 2, 2 };
+        int n, min1 = 0, min2 = 1, sum, minimum;
 
-	
-	}
-	    void findIfArrayIsASubset(int[] main, int[] sub) {
-	    int count = 0;
-	    for (int i = 0; i < main.length; i++) {
-	        for (int j = 0; j < sub.length; j++) {
-	            if (main[i] == sub[j]) {
-	                main[i] = -1;
-	                count++;
-	                break;
-	            }
-	        }
-	    }
-	    if (count == sub.length)
-	        System.out.println("is a subset");
-	    else
-	        System.out.println("is not a subset");
-	    }}
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements you want:");
+
+        n = s.nextInt();
+
+        int a[] = new int[n];
+
+        System.out.println("Enter all the numbers:");
+
+        for(int i = 0; i < n; i++)
+
+        {
+
+            a[i] = s.nextInt();
+
+        }
+
+        minimum=Math.abs(a[0] + a[1]);
+
+        for(int i = 0; i < n; i++)
+
+        {
+
+            for(int j = i + 1; j < n; j++)
+
+            {
+
+                sum = Math.abs(a[i] + a[j]);
+
+                if(sum < minimum)
+
+                {
+
+                    min1 = i;
+
+                    min2 = j;
+
+                    minimum = sum;
+
+                }
+
+            }
+
+        }
+
+        System.out.println("Element 1:"+a[min1]); 
+
+        System.out.println("Element 2:"+a[min2]);
+
+    }
+
+}
