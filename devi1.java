b36bfcbf5a284d1c7681dbd88cb17dd875858d1b
KANCHANADEVI38 <30399523+KANCHANADEVI38@users.noreply.github.com>
2017-07-24 13:59:52 +0530

    Add files via upload

---
+++ devi1.java
@@ -0,0 +1,28 @@
+package sample;
+import java.util.Scanner;
+import java.util.HashSet;
+public class devi1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String longestPrefix = null;
+		  if(strs.length>0){
+		    	String[] strs;
+		        longestPrefix = strs[0];
+		    }
+		    for(int i=1; i<strs.length; i++){
+		    	String[] strs;
+		        String analyzing = strs[i];
+		        int j=0;
+		        for(; j<Math.min(longestPrefix.length(), strs[i].length()); j++){
+		            if(longestPrefix.charAt(j) != analyzing.charAt(j)){
+		                break;
+		            }
+		        }
+		        longestPrefix = strs[i].substring(0, j);
+		    }
+		    return;
+		}}
+	
+
+
